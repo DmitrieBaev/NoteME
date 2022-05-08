925161021b8f1ca0aed886f76ead180109dc3901
Quotoms <azo.cw@yandex.ru>
2022-05-08 17:35:53 +0300

    Добавление данных

--- controller.py
+++ controller.py
@@ -1,5 +1,21 @@
-from model import Note
+from model import Note, db
+from flask import redirect
 
 
-def create_note(title: str, tag: str, body: str):
-    pass
+def cntl_create_note(title: str, tag: str, body: str):
+    """
+    Создание новой заметки
+
+    :param title: Название заметки
+    :param tag: Метка для заметки
+    :param body: Текст заметки
+    """
+    note = Note(n_title=title, n_tag=tag, n_body=body)
+    try:
+        db.session.add(note)
+        db.session.commit()
+    except Exception:
+        # Добавить обработку исключений
+        pass
+    finally:
+        return redirect('/')
--- view.py
+++ view.py
@@ -1,4 +1,6 @@
 from main import app
+from controller import *
+
 from flask import render_template, request
 
 
@@ -10,6 +12,8 @@ def index():
 @app.route('/create-note', methods=['POST', 'GET'])
 def create_note():
     if request.method == 'POST':
-        pass
+        cntl_create_note(request.form['title'],
+                         request.form['tag'],
+                         request.form['text'])
     else:
         return render_template('create-note.html')
