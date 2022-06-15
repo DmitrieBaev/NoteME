356ed3696fada2ad2ea33512251fb42d893ad401
Dmitrie Baew <azo.cw@yandex.ru>
2022-06-15 19:45:54 +0300

    Модель из приложения notes зарегистрирована в админке

--- notes/admin.py
+++ notes/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 
-from .models import Note, Category, User
+from .models import Note, Category
 
 
-admin.site.register(Note)
-admin.site.register(Category)
-admin.site.register(User)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'is_pinned', 'category', 'preview', 'body', 'is_deleted', 'created_at', 'updated_at')
+    list_display_links = ('pk', 'title')
+    list_editable = ('is_pinned', 'is_deleted')
+    list_filter = ('is_pinned', 'is_deleted', 'category')
+    search_fields = ('title',)
+    
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title')
+    list_display_links = ('pk', 'title')
+    search_fields = ('title',)
+
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Category, CategoryAdmin)
