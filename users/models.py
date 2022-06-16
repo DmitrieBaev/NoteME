f606159757816709dcf9bc818842724f19d2d913
Dmitrie Baew <azo.cw@yandex.ru>
2022-06-16 10:59:49 +0300

    Правки в модели Авторов

--- users/models.py
+++ users/models.py
@@ -16,6 +16,13 @@ class Author(AbstractUser):
     avatar = models.ImageField(upload_to=user_directory_path, blank=True,
                                width_field=500, height_field=500,
                                verbose_name='Аватар')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
 
     def __str__( self ):
         return self.username
+    
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ('-created_at',)
