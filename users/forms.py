9b9edfd1984cf4ad4ae731c20c2746b6a45aba21
Dmitrie Baew <azo.cw@yandex.ru>
2022-06-17 20:58:22 +0300

    Добавлены поля Имя/Фамилия в форму регистрации

--- users/forms.py
+++ users/forms.py
@@ -5,17 +5,20 @@ from django.contrib.auth import models as auth_models, forms as auth_forms
 class SignUpForm(auth_forms.UserCreationForm):
     """ Форма регистрации пользователей """
     
-    username = forms.CharField(label='Логин:', max_length=25, widget=forms.TextInput(attrs={ 'class': 'form-control form-control-sm' }),
+    username = forms.CharField(label='Логин:', max_length=25, widget=forms.TextInput(attrs={ 'class': 'form-control form-control-sm', 'autocomplete': 'off' }),
                                help_text='<small class="form-text text-muted">Максимум 25 символов. Допускаются только буквы, цифры и символы @.+-_</small>')
     email = forms.EmailField(label='Эл. адрес:', widget=forms.EmailInput(attrs={ 'class': 'form-control form-control-sm' }),
-                             help_text='<small class="form-text text-muted">Убедитесь, что указываете адрес, к которому у Вас есть доступ.</small>')
+                             help_text='<small class="form-text text-muted">Убедитесь, что указываете адрес, к которому у Вас есть доступ.</small>',
+                             required=True)
     password1 = forms.CharField(label='Пароль:', widget=forms.PasswordInput(attrs={ 'class': 'form-control form-control-sm' }))
     password2 = forms.CharField(label='Подтверждение пароля:', widget=forms.PasswordInput(attrs={ 'class': 'form-control form-control-sm' }),
                                 help_text='<small class="form-text text-muted">Для подтверждения введите, пожалуйста, пароль повторно.</small>')
+    first_name = forms.CharField(label='Ваше имя:', widget=forms.TextInput(attrs={'class': 'form-control form-control-sm', 'autocomplete': 'off'}), required=True)
+    last_name = forms.CharField(label='Ваша Фамилия:', widget=forms.TextInput(attrs={'class': 'form-control form-control-sm', 'autocomplete': 'off'}), required=True)
     
     class Meta:
         model = auth_models.User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
 
 
 class U4AChangeForm(auth_forms.UserChangeForm):
