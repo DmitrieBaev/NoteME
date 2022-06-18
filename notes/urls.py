99520051ffc2e51f27be5ed40259a82f98f51c20
Dmitrie Baew <azo.cw@yandex.ru>
2022-06-18 22:00:56 +0300

    Прописаны url`ы для созданных контроллеров

--- notes/urls.py
+++ notes/urls.py
@@ -3,13 +3,14 @@ NoteME (APP) URL Configuration
 """
 from django.urls import path
 
-from .views import NoteDetail, NotesList, NoteCreate, NoteUpdate, NoteDelete
+from .views import NoteDetail, NotesList, NotesByCategoryList, NoteCreate, NoteUpdate, NoteDelete
 
 
 urlpatterns = [
-    path('',            NotesList.as_view(),    name='notes'),
-    # path('<int:pk>',    NoteDetail.as_view(),   name='note_detail'),
-    # path('new/',        NoteCreate.as_view(),   name='note_create'),
-    # path('edit/',       NoteUpdate.as_view(),   name='note_update'),
-    # path('delete/',     NoteDelete.as_view(),   name='note_delete'),
+        path('',            NotesList.as_view( ),   name='notes'),
+        path('<int:pk>',    NoteDetail.as_view(),   name='note_detail'),
+        path('new/',        NoteCreate.as_view( ),  name='note_create'),
+        path('category/<int:category_id>', NotesByCategoryList.as_view( ), name='category')
+        # path('edit/',       NoteUpdate.as_view(),   name='note_update'),
+        # path('delete/',     NoteDelete.as_view(),   name='note_delete'),
 ]
