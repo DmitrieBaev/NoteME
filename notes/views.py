a05b02efa7b07799c8b0caf8aefab2727dccd730
quotoms <azo.cw@yandex.ru>
2022-07-13 23:20:46 +0300

    Добавлено переопределение получение списка заметок. Пользователь может получить только свои Заметки.

--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,29 @@
 """ Views for notes app """
 
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, response
 
 from .models import Note
 from .permissions import NotesCustomPermissions
 from .serializers import NoteSerializer
 
 
-class NotesViewSet(viewsets.ModelViewSet):
+class NotesViewSet(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     permission_classes = (NotesCustomPermissions,)
+
+    def list(self, request, *args, **kwargs):
+        """
+        Override get list method. Authorized user can only receive their notes.
+        """
+        return response.Response(
+            self.get_serializer(
+                Note.objects.filter(created_by=self.request.user),
+                many=True
+            ).data
+        )
