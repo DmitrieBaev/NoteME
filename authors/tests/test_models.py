53a30dc988c4fb5ebc468ff118304bc9f6653d04
quotoms <azo.cw@yandex.ru>
2022-07-19 20:34:24 +0300

    Покрытие модели Профиля тестами.

--- authors/tests/test_models.py
+++ authors/tests/test_models.py
@@ -3,7 +3,7 @@ from datetime import date
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from authors import models
+from authors.models import Profile
 
 
 class ProfileModelTest(TestCase):
@@ -15,10 +15,9 @@ class ProfileModelTest(TestCase):
                                   password='asdz-drqw132.qdQQWE',
                                   last_name='von Everec',
                                   first_name='Olgierd')
-        models.Profile.objects.create(user=usr,
-                                      bday=date(1272, 11, 9))
+        Profile.objects.create(user=usr, bday=date(1272, 11, 9))
 
-    def test_note_string_representation(self):
-        """ Правильное строковое представление """
-        usr = models.Profile.objects.filter(user__username='tester')
+    def test_profile_string_representation_and_relationship_with_contribauthmodelsuser(self):
+        """ Правильное строковое представление & связь с contrib.auth.models.User """
+        usr = Profile.objects.filter(user__username='tester')
         self.assertEquals(f'tester profile <Olgierd von Everec (1272-11-09)>', str(usr[0]))
