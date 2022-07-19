e72845760787b571f5b41020771f5fe68ffca3a0
quotoms <azo.cw@yandex.ru>
2022-07-19 14:22:30 +0300

    Изменен способ сериализации текущего пользователя. Добавлен комментарий для манипулюций над m2m Category-Note.

--- notes/serializers.py
+++ notes/serializers.py
@@ -1,6 +1,5 @@
 """ Custom serializers for note app """
 
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from .models import Note, Category
@@ -14,41 +13,16 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class NoteSerializer(serializers.ModelSerializer):
-    # @property
-    # def _user(self):
-    #     request = self.context.get('request', None)
-    #     if request:
-    #         print('='*25)
-    #         print(f'{request.user.username}')
-    #         print('='*25)
-    #         return request.user.username
-
-    # created_by = serializers.PrimaryKeyRelatedField(
-    #     read_only=False,
-    #     queryset=User.objects.filter(username=_user),
-    #     default=serializers.CurrentUserDefault()
-    # )
     created_by = serializers.HiddenField(
         default=serializers.CurrentUserDefault()
     )
     preview = serializers.ImageField(required=False)
-    # categories = CategorySerializer(source='category', many=True, read_only=True)
-    # category = serializers.ListSerializer(child=serializers.CharField())
-    # category = serializers.SlugRelatedField(many=True, read_only=True, slug_field='notion')
-
-    # def create(self, validated_data):
-    #     category_data = validated_data.pop('category', [])
-    #     note = Note.objects.create(**validated_data)
-    #     for _category in category_data:
-    #         ctgry = Category.objects.filter(notion=_category.get('notion'))
-    #         # al.name=allergy.get('name')
-    #         note.allergies.add(ctgry)
-    #
-    #     note.save()
-    #     return note
-
-    # def update(self, instance, validated_data):
-
+    # category field has Many2Many relationship
+    # for creating or updating categories in the note
+    # json request can be like:
+    # {...
+    # "category": [1, 13, 17],
+    # ...}
 
     class Meta:
         model = Note
