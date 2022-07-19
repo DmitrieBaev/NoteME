3680c0cc9535c92d0b42ffe7fb0975236ee2f6a8
quotoms <azo.cw@yandex.ru>
2022-07-19 16:53:26 +0300

    Обновлены урлы.

--- NoteME/urls.py
+++ NoteME/urls.py
@@ -9,10 +9,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls')),
+    path('auth/', include('rest_framework.urls')),  # .../auth/login/ and .../auth/logout/
 
-    path('api/v1/auth/', include('djoser.urls')),
-    path('api/v1/auth/', include('djoser.urls.authtoken')),  # token/login/ and token/logout
+    path('api/v1/', include('djoser.urls')),  # .../users/ (Get, Post) and .../users/<pk>/ (Get, Put, Patch, Delete)
+    path('api/v1/auth/', include('djoser.urls.authtoken')),  # .../token/login/ and .../token/logout
 
     path('api/v1/', include('notes.urls')),  # ^api/v1/notes/...
     path('api/v1/', include('authors.urls')),  # ^api/v1/profile/...
