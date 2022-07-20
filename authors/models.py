05c39bbde5f403e5ad6f73b621e4991c15fe5417
quotoms <azo.cw@yandex.ru>
2022-07-20 09:13:33 +0300

    Сигналы для создания Профиля после регистрации Пользователя вынесены в отдельный файл.

--- authors/models.py
+++ authors/models.py
@@ -2,9 +2,6 @@
 
 from django.contrib.auth.models import User
 from django.db import models
-from djoser.signals import user_registered  # Importing Djoser signal
-# from django.db.models.signals import post_save  # Importing django signal
-from django.dispatch import receiver
 
 
 def upload_to(instance, filename):
@@ -25,32 +22,3 @@ class Profile(models.Model):
         verbose_name = 'Профиль'
         verbose_name_plural = 'Профили'
         ordering = ('pk',)
-
-
-# using a decorator, we will describe a trigger for creating a user profile after creating the user itself
-@receiver(user_registered, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(user_registered, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
-# in database (postgresql) it should look like:
-# -------------------------------------------------------------
-# create or replace function create_profile_on_user_insert()
-# returns trigger as $$
-# begin
-    # insert into public.authors_profile(user_id, bday, avatar)
-    # values(new.id, null, null);
-    # return new;
-# end;
-# $$ language 'plpgsql' volatile
-# cost 100;
-#
-# create trigger create_profile_on_user_insert_trigger
-# after insert on public.auth_user
-# for each row
-# execute procedure create_profile_on_user_insert();
